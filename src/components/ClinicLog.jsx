10996f79c0e683405bc40273452b30709cb2a33e
HyeJeong <hj400820908@gmail.com>
2024-04-10 12:17:51 +0900

    CSS 추가

--- src/components/ClinicLog.jsx
+++ src/components/ClinicLog.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
+import ClinicTodayList from './ClinicTodayList'
+import ClinicText from './ClinicText'
+import ClinicEdit from './ClinicEdit'
+import ClinicButtons from './ClinicButtons'
+
 
 const ClinicLog = () => {
   return (
-    <div>ClinicLog
-      
-    </div>
+    <>
+      <ClinicContainer className='clinic-container'>
+        <ClinicTodayList></ClinicTodayList>
+        <ClinicEdit></ClinicEdit>
+        <ClinicText></ClinicText>
+      </ClinicContainer>
+      <ClinicButtons />
+    </>
   )
 }
 
 export default ClinicLog
+
+const ClinicContainer = styled.div`
+  display: flex;
+  max-width: 1440px;
+  height: 90vh;
+  margin: auto;
+`
