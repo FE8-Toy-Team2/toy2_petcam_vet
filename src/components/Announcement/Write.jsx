024f3f99438b0b5efe5fd25236bc4eab3e0d5388
Heejun Byeon <hejuby@gmail.com>
2024-04-15 16:50:09 +0900

    feat: Announcement Write 컴포넌트 제목 추가

--- src/components/Announcement/Write.jsx
+++ src/components/Announcement/Write.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
+import styled from "styled-components";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+
+const WriteTitle = styled.h2`
+  font-size: 4rem;
+  padding: 48px 0;
+`;
 
 const Write = () => {
   const [value, setValue] = useState("");
 
   return (
     <>
-      <h1>Write</h1>
+      <WriteTitle>새 글 쓰기</WriteTitle>
       <ReactQuill theme="snow" value={value} onChange={setValue} />
     </>
   );
