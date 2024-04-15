3b4169da1575c5b4377a5e647eb3d2dc7800d066
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2024-04-15 15:39:11 +0900

    refactor : 📚 root 변수 추가 , button 스타일 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,26 @@ import "./font/font.css";
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
+  :root{
+    --color-prime: #FFCD29;
+    --color-black: #3D3939;
+    --color-white: #FFFFFF;
+    --color-gray-1: #F6F6F6;
+    --color-gray-2: #EEEEEE;
+    --color-gray-3: #D9D9D9;
+    --color-salgu: #F9F4F0;
+    --color-brown: #504239;
+
+    --font-weight-light: 400;
+    --font-weight-bold: 700;
+
+    --font-size-XS: 0.8rem;
+    --font-size-S: 0.9rem;
+    --font-size-M: 1rem;
+    --font-size-L: 1.1rem;
+    --font-size-XL: 1.2rem;
+    --font-size-XXL: 1.5rem;
+  }
   body{
     font-family: "Pretendard";
   }
