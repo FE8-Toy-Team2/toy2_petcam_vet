15bd201ac31112d686f7081b404a645ac474c609
Heejun Byeon <hejuby@gmail.com>
2024-04-16 14:58:02 +0900

    feat: WriteEditor 컴포넌트 Lexical 이용한 구현 (텍스트박스, 히스토리)

--- src/components/Announcement/WriteEditor.jsx
+++ src/components/Announcement/WriteEditor.jsx
@@ -1,20 +1,62 @@
-import { EditorProvider, FloatingMenu, BubbleMenu } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
+import {$getRoot, $getSelection} from "lexical";
+import {useEffect} from "react";
 
-// define your extension array
-const extensions = [
-  StarterKit,
-]
+import {AutoFocusPlugin} from "@lexical/react/LexicalAutoFocusPlugin";
+import {LexicalComposer} from "@lexical/react/LexicalComposer";
+import {RichTextPlugin} from "@lexical/react/LexicalRichTextPlugin";
+import {ContentEditable} from "@lexical/react/LexicalContentEditable";
+import {HistoryPlugin} from "@lexical/react/LexicalHistoryPlugin";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+import styled from "styled-components";
 
-const content = '<p>Hello World!</p>'
+const WriteEditorWrapper = styled.section`
+  position: relative;
+`;
+
+const ContentStyle = {
+  height: "16rem",
+  padding: "1rem",
+  border: "1px solid var(--color-black)",
+  borderRadius: "10px"
+};
+
+const EditorPlaceholder = styled.p`
+  position: absolute;
+  top: 1rem;
+  left: 1rem;
+  color: #666;
+`;
+
+const theme = {
+  // Theme styling goes here
+}
+
+// Catch any errors that occur during Lexical updates and log them
+// or throw them as needed. If you don"t throw them, Lexical will
+// try to recover gracefully without losing user data.
+const onError = (error) => {
+  console.error(error);
+};
 
 const WriteEditor = () => {
+  const initialConfig = {
+    namespace: "MyEditor",
+    theme,
+    onError,
+  };
+
   return (
-    <EditorProvider extensions={extensions} content={content}>
-      <FloatingMenu>This is the floating menu</FloatingMenu>
-      <BubbleMenu>This is the bubble menu</BubbleMenu>
-    </EditorProvider>
-  )
-}
+    <WriteEditorWrapper>
+      <LexicalComposer initialConfig={initialConfig}>
+        <RichTextPlugin
+          contentEditable={<ContentEditable style={ContentStyle} />}
+          placeholder={<EditorPlaceholder>여기에 내용을 입력해 주세요.</EditorPlaceholder>}
+          ErrorBoundary={LexicalErrorBoundary}
+        />
+        <HistoryPlugin />
+      </LexicalComposer>
+    </WriteEditorWrapper>
+  );
+};
 
 export default WriteEditor;
