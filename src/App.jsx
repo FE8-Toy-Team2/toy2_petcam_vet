8b49cca8d2e95c097903d7fad8121992e7e023c3
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2024-04-19 18:12:20 +0900

    import 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import React, { useState } from "react";
 import reset from "styled-reset";
 import { createGlobalStyle } from "styled-components";
