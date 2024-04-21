30ff8fed9044cade2af2907245c18a3d95d2f12f
scripto1 <heeyongi@gmail.com>
2024-04-21 17:23:07 +0900

    fix:회원가입수정

--- src/components/LoginSignup/Signup.jsx
+++ src/components/LoginSignup/Signup.jsx
@@ -10,20 +10,22 @@ const Signup = ({ onLogin, isLoggedIn }) => {
   const [password, setPassword] = useState('');
 	const [confirmPassword, setConfirmPassword] = useState('');
 
-	const validatePassword = (password) => {
-    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*[^\s]).{8,}$/;
-    return passwordRegex.test(password);
-  };
+	
 
   const handleSignup = async (event) => {
     event.preventDefault();
 		
+		const validatePassword = (password) => {
+			const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*[^\s]).{8,}$/;
+			return passwordRegex.test(password);
+		};
+
+		if (!validatePassword(password)) {
+			alert('비밀번호는 최소 8자 이상이어야 하며, 대소문자, 숫자, 특수문자를 포함해야 합니다.');
+			return;
+		}
 		if (password !== confirmPassword) {
       alert('비밀번호가 일치하지 않습니다');			
-      return;
-    }
-		if (!validatePassword(password)) {
-      alert('비밀번호는 최소 8자 이상이어야 하며, 대소문자, 숫자, 특수문자를 포함해야 합니다.');
       return;
     }
 
@@ -52,14 +54,12 @@ const Signup = ({ onLogin, isLoggedIn }) => {
     try {
       const userCredential = await signInWithPopup(auth, provider);
       const user = userCredential.user;
-      // alert(`${user.email}님이 로그인하셨습니다`)
-			Swal.fire({
+      Swal.fire({
         text: `${user.email}님이 로그인하셨습니다`
       });
 			onLogin()
     } catch (error) {
-      const errorCode = error.code;
-      const errorMessage = error.message;      
+      console.error('Error signing up:', error);      
     }
   };
 
