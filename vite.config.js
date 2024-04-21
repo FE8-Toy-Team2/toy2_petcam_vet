e3816d412de6f6705f7f8ee0b748ccd775cddea8
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2024-04-21 22:12:33 +0900

    의존성 수정

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  esbuild: {
-    target: "es2022", // 혹은 'chrome90', 'firefox90' 등 최신 브라우저로 변경
+  build: {
+    target: "ES2022",
   },
 });
